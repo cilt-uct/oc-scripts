741bfb066563fb28a90ab77d4d349ae8fe646370
Stephen Marquard <stephen.marquard@uct.ac.za>
2019-01-22 11:01:07 +0200

    Corrected push

--- push.sh
+++ push.sh
@@ -5,7 +5,7 @@ branch_name=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
 read -p "Branch [$branch_name]: " branch
 branch=${branch:-$branch_name}
 
-read -p "Github Username: " username
+read -p "Github Username (not email): " username
 
 git push https://$username@github.com/cilt-uct/oc-scripts.git $branch
 
