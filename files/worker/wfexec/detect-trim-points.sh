f04d6917890f643a62623ce35d61c10f8726ab20
Corne Oosthuizen <corne.oosthuizen@uct.ac.za>
2019-07-24 09:15:20 +0200

    NOJIRA: Remove m209 from automatic trimming because it is running a captioning pilot course.

--- files/worker/wfexec/detect-trim-points.sh
+++ files/worker/wfexec/detect-trim-points.sh
@@ -40,8 +40,8 @@ OPTIND=1         # Reset in case getopts has been used previously in the shell.
 media=""
 output_file=""
 
-# List of valid venues to use
-venue_list=("hahn4" "m209" "hoerilt1" "hoerilt2" "em4" "alex1a" "anatlt1" "beattielt" "cs302" "snapelt1" "cengsem" "menz12" "ls3a" "ls1a")
+# List of valid venues to use - remove temp [ m209 ]
+venue_list=("hahn4" hoerilt1" "hoerilt2" "em4" "alex1a" "anatlt1" "beattielt" "cs302" "snapelt1" "cengsem" "menz12" "ls3a" "ls1a")
 
 #--start-speech (seconds)    Threshold for speech at the start of the recording [sec] (default: 3)
 start_speech=3
