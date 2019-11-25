4581fa0e28b53b1512d2a855e06df394b883a039
Ngonidzashe Nicholas Choga <ngonidzashe.choga@uct.ac.za>
2019-11-25 11:53:12 +0200

    OPENCAST-1441 empty venue detection (#13)
    * OPENCAST-1441: Initial commit for executing vad python script.
    
    * OPENCAST-1441 - added empty venue detection workflow
    
    * OPENCAST-1441 - make file executable
    
    * OPENCAST-1441 - update to script
    
    * OPENCAST-1441 - remove snapshot from workflow
    
    * OPENCAST-1441 - update workflow
    
    * OPENCAST-1441 - added operation to the auto-trim workflow
    
    * OPENCAST-1441 - add empty venue comment after processing
    
    * OPENCAST-1441 - deleted temp workflow
    
    * OPENCAST-1441 - update command
    
    * OPENCAST-1441 - empty venue key naming updated
    
    * OPENCAST-1441 -  created new empty venue workflow to run before other workflows (Still needs testing)
    
    * OPENCAST-1441 - Updated logic for empty venue detection
    
    * OPENCAST-1441 -  execute many test
    
    * OPENCAST-1441 - allow script to fail gracefully if not a flac file
    
    * OPENCAST-1441 Test workflow and script for execute many setting output properties
    
    * OPENCAST-1441 - add the set-workflow-properties config on empty venue detection workflow
    
    * OPENCAST-1441 - remove exit code when flac file passed to script
    
    * OPENCAST-1441 - Update check in script
    
    * OPENCAST-1441 - update check in script and create output file on unknown format
    
    * Test script for tracks with audio only
    
    * OPENCAST-1441 - Add source audio config

---
+++ files/worker/wfexec/detect-empty-venue.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Initialize our variables:
+media=""
+output_file=""
+
+# -d, --debug   If the flag exists then output debug file
+DEBUG=false
+
+OPTIONS=o:i:d
+LONGOPTIONS=debug,input:,output:
+
+# -temporarily store output to be able to check for errors
+# -e.g. use “--options” parameter by name to activate quoting/enhanced mode
+# -pass arguments only via   -- "$@"   to separate them correctly
+PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTIONS --name "$0" -- "$@")
+if [[ $? -ne 0 ]]; then
+    # e.g. $? == 1
+    #  then getopt has complained about wrong arguments to stdout
+    usage $@
+    exit 2
+fi
+# read getopt’s output this way to handle the quoting right:
+eval set -- "$PARSED"
+
+# now enjoy the options in order and nicely split until we see --
+while true; do
+    case "$1" in
+        -i|--input)
+            media="$2"
+            shift 2
+            ;;
+        -o|--output)
+            output_file="$2"
+            shift 2
+            ;;
+        -d|--debug)
+            DEBUG=true
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Programming error"
+            exit 3
+            ;;
+    esac
+done
+
+# handle non-option arguments
+
+if [ -z "$media" ] && [ -z "$output_file" ]; then
+    echo "Required: input and output file (-i /path/to/file-media-wav -o /path/to/output.txt)"
+    exit 4
+fi
+
+if [[ $media == *".flac"* ]] || [[ $media == *".mp4"* ]] || [[ $media == *".mkv"* ]];
+    then
+        python3 /opt/VAD-python/detectVoiceInWave.py -i $media -o $output_file
+    else
+        echo "'$media' has an unknown format."
+        echo > $output_file
+fi
