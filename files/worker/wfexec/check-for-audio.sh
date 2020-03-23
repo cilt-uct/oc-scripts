bde0930c997ec0f3755ea7a8e41809d76b4e9664
NgoniChoga <CHGNGO001@myuct.ac.za>
2020-03-23 22:10:36 +0200

    clean up file

--- files/worker/wfexec/check-for-audio.sh
+++ files/worker/wfexec/check-for-audio.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Initialize our variables:
-media="77.webm"
-output_file="77.mp4"
+media=""
+output_file=""
 
 # -d, --debug   If the flag exists then output debug file
 DEBUG=false
@@ -14,40 +14,40 @@ LONGOPTIONS=debug,input:,output:
 # -e.g. use “--options” parameter by name to activate quoting/enhanced mode
 # -pass arguments only via   -- "$@"   to separate them correctly
 # PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTIONS --name "$0" -- "$@")
-# if [[ $? -ne 0 ]]; then
-#     # e.g. $? == 1
-#     #  then getopt has complained about wrong arguments to stdout
-#     usage $@
-#     exit 2
-# fi
-# # read getopt’s output this way to handle the quoting right:
-# eval set -- "$PARSED"
+if [[ $? -ne 0 ]]; then
+    # e.g. $? == 1
+    #  then getopt has complained about wrong arguments to stdout
+    usage $@
+    exit 2
+fi
+# read getopt’s output this way to handle the quoting right:
+eval set -- "$PARSED"
 
-# # now enjoy the options in order and nicely split until we see --
-# while true; do
-#     case "$1" in
-#         -i|--input)
-#             media="$2"
-#             shift 2
-#             ;;
-#         -o|--output)
-#             output_file="$2"
-#             shift 2
-#             ;;
-#         -d|--debug)
-#             DEBUG=true
-#             shift
-#             ;;
-#         --)
-#             shift
-#             break
-#             ;;
-#         *)
-#             echo "Programming error"
-#             exit 3
-#             ;;
-#     esac
-# done
+# now enjoy the options in order and nicely split until we see --
+while true; do
+    case "$1" in
+        -i|--input)
+            media="$2"
+            shift 2
+            ;;
+        -o|--output)
+            output_file="$2"
+            shift 2
+            ;;
+        -d|--debug)
+            DEBUG=true
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Programming error"
+            exit 3
+            ;;
+    esac
+done
 
 # handle non-option arguments
 
