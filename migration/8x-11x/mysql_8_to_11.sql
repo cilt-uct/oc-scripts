c47072f1fc808b5d97623d308631cc0f953493a4
Nosipho Mali <yheyhePrec@gmail.com>
2021-11-18 15:12:55 +0200

    check if tables exist before dropping them

--- migration/8x-11x/mysql_8_to_11.sql
+++ migration/8x-11x/mysql_8_to_11.sql
@@ -1,6 +1,6 @@
 -- Drop unused tables
-drop table oc_job_oc_service_registration;
-drop table oc_job_context;
+drop table if exists oc_job_oc_service_registration;
+drop table if exists oc_job_context;
 
 -- Increase mime_type field size
 --ALTER TABLE oc_assets_asset MODIFY COLUMN mime_type VARCHAR (255);
