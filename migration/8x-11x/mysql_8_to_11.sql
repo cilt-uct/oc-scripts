5c30cb44b7a3b588d47b141a634c16a227b9eae4
Nosipho Mali <yheyhePrec@gmail.com>
2021-11-18 13:02:35 +0200

    OPENCAST-3073: Drop unused tables

---
+++ migration/8x-11x/mysql_8_to_11.sql
@@ -0,0 +1,7 @@
+-- Drop unused tables
+drop table oc_job_oc_service_registration;
+drop table oc_job_context;
+
+-- Increase mime_type field size
+--ALTER TABLE oc_assets_asset MODIFY COLUMN mime_type VARCHAR (255);
+--this change has already been applied.
